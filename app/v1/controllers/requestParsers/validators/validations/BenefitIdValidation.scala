4646b569cff18b805b2abdb7f8820d95d5a3dd61
Narendra Vijayarao <narendra.vijayarao@digital.hmrc.gov.uk>
2020-08-19 10:13:25 +0100

    resolve the conflicts

--- app/v1/controllers/requestParsers/validators/validations/BenefitIdValidation.scala
+++ app/v1/controllers/requestParsers/validators/validations/BenefitIdValidation.scala
@@ -21,7 +21,7 @@ import v1.models.errors.{BenefitIdFormatError, MtdError}
 object BenefitIdValidation {
 
   def validate(benefitId: String): List[MtdError] = {
-    val regex = "^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$"
+    val regex = "^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$"
     if (benefitId.matches(regex)) NoValidationErrors else List(BenefitIdFormatError)
   }
 }
